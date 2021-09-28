4455600571b1bac68a8926488de56c8da5ed3a12
Dairo Barrios Ramos <dairo.barrios.ramos@gmail.com>
2021-09-28 16:18:45 -0500

    fin seccion 5

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { EMPTY } from 'rxjs';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+
   }
+
+  
 
 }
